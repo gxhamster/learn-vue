2fddbb8d5c7ccdf7007bc603c4823e0b4277cb0e
Jhon Arthur <ja@pop-os.localdomain>
2020-06-21 22:52:46 +0500

    Added list and learned v-for

--- intro-vue/main.js
+++ intro-vue/main.js
@@ -2,12 +2,36 @@ var app = new Vue(
     {
         el: '#app',
         data: {
-            product: 'socks',
+            product: 'Socks',
             image: './vmSocks-green-onWhite.jpg',
             description: 'A pair of fuzzy',
             link: "https://github.com/gxhamster/learn-vue",
             inventory: 10,
-            inStock: true  
+            items: [
+                {
+                    name: 'Teddy Bear',
+                    color: 'Brown',
+                    size: 10
+                },
+                {
+                    name: 'Pillow',
+                    color: 'White',
+                    size: 11
+                },
+                {
+                    name: 'Umbrella',
+                    color: 'Blue',
+                    size: 14
+
+                },
+                {
+                    name: 'Tablet',
+                    color: 'Green',
+                    size: 7
+                },
+
+            ],
+            onSale: false
         }
     }
 )
