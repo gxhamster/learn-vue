af7daa68288f5e871a9f5b771e1fd5fa5c84af2e
Jhon Arthur <ja@pop-os.localdomain>
2020-06-21 22:00:33 +0500

    - Added stylesheet, and sock image - Added product-image

--- intro-vue/main.js
+++ intro-vue/main.js
@@ -2,7 +2,8 @@ var app = new Vue(
     {
         el: '#app',
         data: {
-            product: 'socks' 
+            product: 'socks',
+            description: 'A pair of fuzzy'  
         }
     }
 )
