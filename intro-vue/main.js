e0a4b62689a6d251333b3f6fad0724caa558cc12
Jhon Arthur <ja@pop-os.localdomain>
2020-06-23 10:47:03 +0500

    Added class style bindings

--- intro-vue/main.js
+++ intro-vue/main.js
@@ -5,7 +5,6 @@ const app = new Vue(
         el: '#app',
         data: {
             product: 'Socks',
-            images: ['./vmSocks-green-onWhite.jpg', './vmSocks-blue-onWhite.jpg'],
             image: './vmSocks-green-onWhite.jpg',
             description: 'A pair of fuzzy',
             link: "https://github.com/gxhamster/learn-vue",
@@ -13,26 +12,17 @@ const app = new Vue(
             items: [
                 {
                     name: 'Teddy Bear',
-                    color: 'Brown',
-                    size: 10
+                    color: 'blue',
+                    size: 10,
+                    image: './vmSocks-blue-onWhite.jpg'
                 },
                 {
                     name: 'Pillow',
-                    color: 'White',
-                    size: 11
+                    color: 'green',
+                    size: 11,
+                    image: './vmSocks-green-onWhite.jpg'
                 },
-                {
-                    name: 'Umbrella',
-                    color: 'Blue',
-                    size: 14
-
-                },
-                {
-                    name: 'Tablet',
-                    color: 'Green',
-                    size: 7
-                },
-
+    
             ],
             onSale: false,
             cart: 0
@@ -41,11 +31,9 @@ const app = new Vue(
             addToCart() {
                 this.cart += 1
             },
-
-            changeImage() {
-                count++;
-                let index = count % this.images.length;
-                this.image = this.images[index];
+            
+            changeImage(itemImage) {
+               this.image = itemImage
             }
         }
     }
